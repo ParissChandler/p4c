dfd306c86defb66158bdc1c5d42d1208f6687c13
Pariss Chandler <parisschandler@yahoo.com>
2017-03-30 17:46:27 -0400

    latest version of this site. Still need to add forms

--- js/endofprep.js
+++ js/endofprep.js
@@ -1,62 +1,62 @@
-$('#navIcon').on('click', function() {
-  $('#links').toggleClass('active');
-});
-
-$(".fair-ness").on("keyup", processInput);
-$("input").on("keyup", processInput);
-
-var allTextAreas = $(".fair-ness");
-var allInputs = $("input");
-
-function textAreaMemory() {
-  Object.keys(allTextAreas).forEach(function(t) {
-    Object.keys(localStorage).forEach(function(ls) {
-      var textArea = allTextAreas[t].name;
-      if(textArea == ls) {  $(`textarea[name=${textArea}]`).html(localStorage[textArea])
-      }
-    })
-  })
-}
-textAreaMemory()
-
-function processInput() {
-  var inputName = this.name;
-  var userInput = this.value;
-  localStorage.setItem(inputName, userInput);
-}
-
-var allInputs = $("input");
-
-$("input").on("keyup", processInput);
-
-console.log(allInputs);
-
-function inputMemory() {
-  Object.keys(allInputs).forEach(function(i) {
-    Object.keys(localStorage).forEach(function(ls) {
-      var inputField = allInputs[i].name;
-      if(inputField == ls) {
-        $(`input[name=${inputField}]`).val(localStorage[inputField]);
-      }
-    })
-  })
-}
-
-inputMemory();
-
-function processInput() {
-  var inputName = this.name;
-  var userInput = this.value;
-  localStorage.setItem(inputName, userInput);
-}
-
-<?php
-$to      = '';
-$subject = 'the subject';
-$message = 'Preparation';
-$headers = 'From: prepare4conversation.com' . "\r\n" .
-    'Reply-To: prepare4conversation@gmail.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-
-mail($to, $subject, $message, $headers);
-?>
+// $('#navIcon').on('click', function() {
+//   $('#links').toggleClass('active');
+// });
+//
+// $(".fair-ness").on("keyup", processInput);
+// $("input").on("keyup", processInput);
+//
+// var allTextAreas = $(".fair-ness");
+// var allInputs = $("input");
+//
+// function textAreaMemory() {
+//   Object.keys(allTextAreas).forEach(function(t) {
+//     Object.keys(localStorage).forEach(function(ls) {
+//       var textArea = allTextAreas[t].name;
+//       if(textArea == ls) {  $(`textarea[name=${textArea}]`).html(localStorage[textArea])
+//       }
+//     })
+//   })
+// }
+// textAreaMemory()
+//
+// function processInput() {
+//   var inputName = this.name;
+//   var userInput = this.value;
+//   localStorage.setItem(inputName, userInput);
+// }
+//
+// var allInputs = $("input");
+//
+// $("input").on("keyup", processInput);
+//
+// console.log(allInputs);
+//
+// function inputMemory() {
+//   Object.keys(allInputs).forEach(function(i) {
+//     Object.keys(localStorage).forEach(function(ls) {
+//       var inputField = allInputs[i].name;
+//       if(inputField == ls) {
+//         $(`input[name=${inputField}]`).val(localStorage[inputField]);
+//       }
+//     })
+//   })
+// }
+//
+// inputMemory();
+//
+// function processInput() {
+//   var inputName = this.name;
+//   var userInput = this.value;
+//   localStorage.setItem(inputName, userInput);
+// }
+//
+// <?php
+// $to      = '';
+// $subject = 'the subject';
+// $message = 'Preparation';
+// $headers = 'From: prepare4conversation.com' . "\r\n" .
+//     'Reply-To: prepare4conversation@gmail.com' . "\r\n" .
+//     'X-Mailer: PHP/' . phpversion();
+//
+// mail($to, $subject, $message, $headers);
+// ?>
